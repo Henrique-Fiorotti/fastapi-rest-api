bb6278665ed2cfeb9b6b59bf27eaf13c30d2c3fe
Henrique-Fiorotti <hberdoldifiorotti@gmail.com>
2026-02-10 11:17:42 -0300

    Refactor code structure for improved readability and organization

--- main.py
+++ main.py
@@ -1,48 +1,55 @@
+# Importation
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional
 
+# Iniciatializing FastAPI
 app = FastAPI()
 
+# Array with users
 users = [
     {"id": 1, "name": "Daniel", "age": 34, "mail": "daniel@gmail.com"},
     {"id": 2, "name": "Alice", "age": 19, "mail": "alice@gmail.com"}
     ]
 
+# Class models
 class UserResponse(BaseModel):
     id: int
     name: str
     age: int
     mail: str
 
-
 class UserPost(BaseModel):
     name: str
     age: int
     mail: str
     password: str
-    
+
 class UserPatch(BaseModel):
     name: Optional[str] = None
     age: Optional[int] = None
     mail: Optional[str] = None
     password: Optional[str] = None
 
+# Verify if API is Working (GET)
 @app.get("/")
 def api():
     return{"message": "API Started"}
 
+# List of all users (GET)
 @app.get("/users", response_model= list[UserResponse])
 def list_users():
     return users
 
+# Search for a specific user (GET)
 @app.get("/user/{id}", response_model= UserResponse)
 def get_user(id: int):
     for user in users:
         if user["id"] == id:
             return user
     raise HTTPException(status_code=404, detail="User not found")
-        
+
+# Create a new user
 @app.post("/user", response_model=UserPost, status_code=status.HTTP_201_CREATED)
 def post_user(usuario: UserPost):
     new_id = max(user["id"] for user in users) + 1
@@ -64,6 +71,7 @@ def post_user(usuario: UserPost):
     users.append(new_user)
     return new_user
 
+# Patch some requested informations in user
 @app.patch("/user/{id}")
 def patch_user(id: int, userPatch: UserPatch):
     
@@ -83,7 +91,7 @@ def patch_user(id: int, userPatch: UserPatch):
             return user
     raise HTTPException(status_code=404, detail="User not found")
         
-
+# Delete user
 @app.delete("/user/{id}")
 def delete_user(id: int):
     for user in users:

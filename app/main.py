9e2ec98d0de6809c6d5e87138f54a25f0cbbf997
Henrique <hberdoldifiorotti@gmail.com>
2026-02-24 23:08:09 -0300

    Confirmação teste

--- app/main.py
+++ app/main.py
@@ -3,3 +3,21 @@ from fastapi import FastAPI
 
 # Iniciatializing FastAPI
 app = FastAPI()
+
+#Alteração teste
+
+# Search for a specific user (GET)
+@app.get("/user/{id}", response_model= UserResponse, status_code=status.HTTP_200_OK)
+def get_user(id: int):
+    for user in users:
+        if user["id"] == id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+
+# Search for a specific user (GET)
+@app.get("/user/{id}", response_model= UserResponse, status_code=status.HTTP_200_OK)
+def get_user(id: int):
+    for user in users:
+        if user["id"] == id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
